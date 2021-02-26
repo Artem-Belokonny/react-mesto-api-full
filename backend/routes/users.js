59cd41f8d9a45aea9181e4d13266f1870eaeff5d
Artem Belokonny <volorel@yandex.ru>
2021-02-26 22:44:40 +0300

    авава

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,15 +2,15 @@ const router = require('express').Router();
 const controller = require('../controllers/users');
 const avatarPatchValidator = require('../validators/avatarPatch');
 const userPatchValidator = require('../validators/avatarPatch');
-// const registerValidator = require('../validators/register');
-// const authValidator = require('../validators/auth');
+const registerValidator = require('../validators/register');
+const authValidator = require('../validators/auth');
 
+router.post('/signin', authValidator, controller.login);
+router.post('/signup', registerValidator, controller.createUser);
 router.get('/', controller.getUsers);
 router.get('/me', controller.getUserInfo);
 router.get('/:id', controller.getUser);
 router.patch('/me', userPatchValidator, controller.updateUser);
 router.patch('/me/avatar', avatarPatchValidator, controller.updateAvatar);
-// router.post('/signin', authValidator, controller.login);
-// router.post('/signup', registerValidator, controller.createUser);
 
 module.exports = router;
