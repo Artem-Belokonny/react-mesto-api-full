6c26948b7de5bc7ec04f3bfc49647c642a0cbf50
Artem Belokonny <volorel@yandex.ru>
2021-02-26 22:29:11 +0300

    ревью

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -2,12 +2,12 @@ const { CelebrateError } = require('celebrate');
 
 const errorHandler = (err, req, res, next) => {
   if (err instanceof CelebrateError) {
-    return res.status(400).send(err.details.get('message'));
+    return res.status(400).send(err.details.get('body'));
   }
   if (err.status) {
     return res.status(err.status).send({ message: err.message });
   }
-  res.status(500).send({ message: err.message });
+  return res.status(500).send({ message: err.message });
 };
 
 module.exports = errorHandler;
