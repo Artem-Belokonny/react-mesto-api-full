17ef7b050fee9cab27fab27dfcf7ea94f447d398
Artem Belokonny <volorel@yandex.ru>
2021-02-26 22:52:49 +0300

    ава

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,8 @@ const userRoutes = require('./users');
 const cardRoutes = require('./cards');
 const authMiddleware = require('../middlewares/auth');
 
-router.use('/users', authMiddleware, userRoutes);
-router.use('/cards', authMiddleware, cardRoutes);
+router.use('/', authMiddleware, userRoutes);
+router.use('/', authMiddleware, cardRoutes);
 router.use('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,15 +2,15 @@ const router = require('express').Router();
 const controller = require('../controllers/users');
 const avatarPatchValidator = require('../validators/avatarPatch');
 const userPatchValidator = require('../validators/avatarPatch');
-const registerValidator = require('../validators/register');
-const authValidator = require('../validators/auth');
+// const registerValidator = require('../validators/register');
+// const authValidator = require('../validators/auth');
 
-router.post('/signin', authValidator, controller.login);
-router.post('/signup', registerValidator, controller.createUser);
-router.get('/', controller.getUsers);
-router.get('/me', controller.getUserInfo);
-router.get('/:id', controller.getUser);
-router.patch('/me', userPatchValidator, controller.updateUser);
-router.patch('/me/avatar', avatarPatchValidator, controller.updateAvatar);
+// router.post('/signin', authValidator, controller.login);
+// router.post('/signup', registerValidator, controller.createUser);
+router.get('users', controller.getUsers);
+router.get('users/me', controller.getUserInfo);
+router.get('users/:id', controller.getUser);
+router.patch('users/me', userPatchValidator, controller.updateUser);
+router.patch('users/me/avatar', avatarPatchValidator, controller.updateAvatar);
 
 module.exports = router;
