a786fc3c5578a5002179c489e0d24102d48b7472
Artem Belokonny <volorel@yandex.ru>
2021-02-26 23:14:13 +0300

    вавыавыа

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const controller = require('../controllers/cards');
 const createCardValidator = require('../validators/cardCreate');
 
-router.get('cards', controller.getCards);
-router.post('cards/', createCardValidator, controller.createCard);
-router.delete('cards/:cardId', controller.deleteCard);
-router.put('cards/:cardId/likes', controller.likeCard);
-router.delete('cards/:cardId/likes', controller.dislikeCard);
+router.get('/', controller.getCards);
+router.post('/', createCardValidator, controller.createCard);
+router.delete('/:cardId', controller.deleteCard);
+router.put('/:cardId/likes', controller.likeCard);
+router.delete('/:cardId/likes', controller.dislikeCard);
 
 module.exports = router;
