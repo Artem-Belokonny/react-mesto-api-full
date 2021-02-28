4aaad8a45846bd3214fc662a64ccb05ef9e7c241
Artem Belokonny <volorel@yandex.ru>
2021-02-28 13:38:44 +0300

    проверка

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,7 +6,7 @@ const userPatchValidator = require('../validators/userPatch');
 router.get('/', controller.getUsers);
 router.get('/me', controller.getUserInfo);
 router.get('/:id', controller.getUser);
-router.patch('/me', userPatchValidator, controller.updateUser);
-router.patch('/me/avatar', avatarPatchValidator, controller.updateAvatar);
+router.patch('/me', controller.updateUser);
+router.patch('/me/avatar', controller.updateAvatar);
 
 module.exports = router;
